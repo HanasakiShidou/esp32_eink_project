7781e5ad8051d588b9c49c0efe6517b1a79d5b55
Petuina <178623265@qq.com>
2026-03-02 00:39:22 +0800

    init commit

---
+++ main/esp32_impl.cpp
@@ -0,0 +1,39 @@
+#include "esp32_impl.hpp"
+
+Esp32Impl::Esp32Impl()
+{
+
+}
+
+int Esp32Impl::DigitalWrite(EPD_IO_PIN pin, uint8_t value) {
+
+}
+
+uint8_t Esp32Impl::DigitalRead(EPD_IO_PIN pin) {
+
+}
+
+int Esp32Impl::SPIWriteByte(uint8_t value) {
+
+}
+
+int Esp32Impl::SPIWriteBytes(uint8_t *pData, uint32_t len) {
+
+}
+
+int Esp32Impl::Delay(uint32_t ms) {
+
+}
+
+int Esp32Impl::Init() {
+
+}
+
+bool Esp32Impl::Available() {
+
+}
+
+Esp32Impl::~Esp32Impl()
+{
+
+}
---
+++ main/esp32_impl.hpp
@@ -0,0 +1,18 @@
+#include "hardwareapi.hpp"
+#include "epd_2in9v2.hpp"
+
+class Esp32Impl : public HardwareAPI
+{
+public:
+    Esp32Impl();
+    int DigitalWrite(EPD_IO_PIN pin, uint8_t value) override;
+    uint8_t DigitalRead(EPD_IO_PIN pin) override;
+    int SPIWriteByte(uint8_t value) override;
+    int SPIWriteBytes(uint8_t *pData, uint32_t len) override;
+    int Delay(uint32_t ms) override;
+    int Init() override;
+    bool Available() override;
+    ~Esp32Impl() override;
+private:
+};
+
